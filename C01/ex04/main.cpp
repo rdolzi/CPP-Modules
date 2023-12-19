305ab2389dddb893116cb23867abacc0dcc432a1
Ruggero Dolzi <rdolzi@e4r5p3.42roma.it>
2023-12-19 23:27:24 +0100

    c01/ex04 done

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -1,11 +1,31 @@
 # include <iostream>
 # include <fstream> 
+# include <string>
+#include <sstream>
 
+void write_word(std::string str, std::ofstream   &outFile, char **argv)
+{
+    std::istringstream iss(str);
+    std::string word;
+    while (iss >> word)
+    {
+        if (word == argv[2])
+            outFile << argv[3];
+        else
+            outFile << word;
+        if (!(iss >> std::ws).eof())
+            outFile << ' ';
+    }
+    outFile << '\n';
+}
+
+// https://math.hws.edu/eck/cs225/s03/files_and_strings.html
 int main(int argc, char **argv)
 {
-    std::ofstream outFile;
-    std::ifstream inFile;
-    char          *name;
+    std::ofstream   outFile;
+    std::ifstream   inFile;
+    std::string     str;
+    std::string     strConcat(argv[1]);
 
     if (argc != 4)
     {
@@ -13,18 +33,24 @@ int main(int argc, char **argv)
         return (1);
     }
     inFile.open(argv[1]);
-    if (inFile.is_open())
+    if (!inFile.is_open())
     {
         std::cerr << "Invalid filename\n";
         return (1);
     }
-    outFile.open((std::string)argv[1] + ".replace");
-    if (!outFile.is_open())
+    strConcat += ".replace";
+    outFile.open(strConcat.c_str());
+    if (!outFile)
     {
         inFile.close();
         std::cerr << "Error during creation file\n";
         return (1);
     }
-    while 
-
+    std::getline(inFile, str, '\n');
+    while (inFile) {
+        write_word(str, outFile, argv);
+        std::getline(inFile,str);  
+    }
+    inFile.close();
+    outFile.close();
 }
